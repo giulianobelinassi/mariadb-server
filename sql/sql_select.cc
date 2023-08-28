f01b9c042258bc47b18d5210d3f6f6bf5edc6609
Giuliano Belinassi <gbelinassi@suse.de>
2023-08-28 17:31:45 -0300

    rebased MDEV-26419

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -27590,7 +27590,8 @@ int setup_order(THD *thd, Ref_ptr_array ref_pointer_array, TABLE_LIST *tables,
       an ORDER BY clause
     */
 
-    if (for_union && (item->with_sum_func() || item->with_window_func()))
+    //if (for_union && (item->with_sum_func() || item->with_window_func()))
+    if (for_union && item->with_sum_func())
     {
       my_error(ER_AGGREGATE_ORDER_FOR_UNION, MYF(0), number);
       return 1;
